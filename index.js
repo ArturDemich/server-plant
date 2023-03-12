2075bfc328e3284d05e5027f80042c51e8acb308
ArturDemich <strelokar@gmail.com>
2023-03-12 13:26:08 +0200

    fix

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import router from './router.js'
-
+import cors from 'cors'
 
 const port = process.env.PORT || 3020
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
@@ -13,6 +14,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS")
     next();
 });
+
 app.use('/api', router)
 
 app.listen(port, () => console.log(`Server start on port ${port}`))
