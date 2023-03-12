0825b633c8d8c2a3ae8e214baa85d607bc4ddecc
ArturDemich <strelokar@gmail.com>
2023-03-12 13:43:15 +0200

    fix

--- index.js
+++ index.js
@@ -19,26 +19,3 @@ app.use('/api', router)
 
 app.listen(port, () => console.log(`Server start on port ${port}`))
 
-/* app.use(express.json())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS")
-    next();
-});
-app.use('/api', router)
-
-async function startApp() {
-    try {
-        await mongoose
-            .set('strictQuery', true)
-            .connect(db)
-            .then((res) => console.log('Connected to DB'))
-            .catch((error) => console.log(error))
-        app.listen(port, () => console.log(`Server start on port ${port}`))
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-startApp() */
