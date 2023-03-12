9d2f0254bfe958ddcd6f1443ba3dd143d854e560
ArturDemich <strelokar@gmail.com>
2023-03-12 15:04:48 +0200

    fix

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const port = process.env.PORT || 3020
 
 
 const app = express()
-app.use(cors({
+/* app.use(cors({
     origin: ['http://localhost:19006']
-}))
+})) */
 app.use(express.json())
-/* app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "*");
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:19006"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type");
     res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS")
     next();
-}); */
+});
 
 app.use('/api', router)
 
