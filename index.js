12f53ec5d75de5161e22e6821231edc94e04c32e
ArturDemich <strelokar@gmail.com>
2023-03-12 11:05:54 +0200

    aded vercel file

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 3020
 const app = express()
 app.use(express.json())
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:19006"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS")
     next();
 });
