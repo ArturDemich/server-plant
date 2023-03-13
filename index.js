72162f33e33ebc14cf3895006ee1878df3ee910e
ArturDemich <strelokar@gmail.com>
2023-03-13 21:48:00 +0200

    fix

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ import cors from 'cors'
 
 const port = process.env.PORT || 3020
 
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
 
 const app = express()
-/* app.use(cors({
-    origin: ['http://localhost:19006']
-})) */
+    / app.use(cors(corsOptions))
 app.use(express.json())
-app.use(function (req, res, next) {
+/* app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:19006"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type");
     res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS")
     next();
-});
+}); */
 
 app.use('/api', router)
 
